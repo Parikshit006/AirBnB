9459c8d6c1817a0d07db8aefc096113a902df772
Parikshit006 <parikshitpatil006@gmail.com>
2026-01-12 22:47:47 +0530

    cloudconfig changes

--- cloudConfug.js
+++ cloudConfug.js
@@ -1,4 +1,4 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "AirBnB-Dev",
-    allowed_formats: ["png", "jpg", "jpeg"],
+    allowedFormats: ["jpeg", "png", "jpg"],
   },
 });
 
